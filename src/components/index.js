1853abcd5e229fe9e4fc1a229fc1bf66daa21845
Benta <bentaakitwi500@gmail.com>
2023-05-04 19:41:38 +0300

    update a file

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "../components/navbar/Header";
+import Services from "./Services";
+import Properties from "./Properties";
+import Search from "./Search";
+import CTA from "./CTA";
+import Testimonials from "./Testimonials";
+import Footer from "./Footer";
+
+export {
+  Header,
+  Services,
+  Properties,
+  Testimonials,
+  Footer,
+  Search,
+  CTA,
+};
